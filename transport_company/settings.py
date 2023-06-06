3d8b591410ba62688bf9e1fb1c15e81078e2d264
egor dyakonov <egordyakonov2000@gmail.com>
2023-06-07 03:17:38 +0900

    wtf

--- transport_company/settings.py
+++ transport_company/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = "django-insecure-yn#=$y$r=s5_sx(j%c-j2!)b!2jx%9=&cm#9-$ocf&699^=7&c"
 
-# import os
+import os
 # SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', 'cg#p$g+j9tax!#a3cup@1$8obt2_+&k3q+pmu)5%asj6yjpkag')
 
 # SECURITY WARNING: don't run with debug turned on in production!
